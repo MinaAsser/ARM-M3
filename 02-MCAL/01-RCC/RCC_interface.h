6e938271d0d3477511873afba3f899690d5c883c
MinaAsser <107791790+MinaAsser@users.noreply.github.com>
2023-02-11 18:39:54 +0200

    Add files via upload

---
+++ 02-MCAL/01-RCC/RCC_interface.h
@@ -0,0 +1,21 @@
+/*********************************************************************/
+/******************************* Author : Mina Asser  ****************/
+/******************************* Date   : 6 FEB 2023  ****************/
+/******************************* VERSION: V01         ****************/
+/******************************* swc : RCC ***************************/
+/*********************************************************************/
+
+#ifndef RCC_INTERFACE_H
+#define RCC_INTERFACE_H
+ /* PERIPHERAL MACROS */
+ #define RCC_AHB         0
+ #define RCC_APB1        1
+ #define RCC_APB2        2
+ void RCC_voidInitSystemClock(void);
+void RCC_voidEnableClock(u8 Copyu8BusId,u8 Copyu8PerId);
+void RCC_voidDisableClock(u8 Copyu8BusId,u8 Copyu8PerId);
+
+
+
+
+#endif
